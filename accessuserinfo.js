a686bc948c34e0e91093ee60a8c214cab40e811a
Cam Frohar <62399634+camfrohar@users.noreply.github.com>
2021-02-06 13:53:37 -0500

    Add files via upload

---
+++ accessuserinfo.js
@@ -0,0 +1,29 @@
+﻿/*Insert:
+ <button onclick = "writeUserData()">Submit</button>
+   into HTML,
+   so that it calls the function(s) defined below.
+*/
+
+
+function writeUserData() {
+    firebase.database().ref("User").set({
+        id: process.env.id,
+        name: document.getElementById("nameField").value,
+        age: document.getElementById("ageField").value,
+        gender: document.getElementById("genderField").value,
+        weight: document.getElementById("weightField").value,
+        height: document.getElementById("heightField").value
+    })
+    getUserData();
+}
+
+
+function getUserData() {
+    firebase.database().ref("User").set({
+        snapshot.forEach(function(childSnapshot))
+        {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+        }
+    })
+}
